59337891010cb3a9cbf01fd92c54f7b36dfe24a6
Dhévy Goffin <Dhevy@MacBook-Air-de-Dhevy.local>
2022-05-25 10:49:03 +0200

    commit now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Box } from "@mui/material";
 import { Container } from "@mui/material";
 import "./App.css";
-import Controller from "./components/Controller";
 import TrackList from "./components/TrackList";
 import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
 
